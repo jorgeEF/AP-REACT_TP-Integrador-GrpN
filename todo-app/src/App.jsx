fd7fe6c1c49e1a9e0b9cdf759f0bb60c41f246cc
JFEMENIA <jorgefemenia@gmail.com>
2023-11-22 00:53:26 -0300

    fix marcar o eliminar tareas

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Spacer, Heading, Text, Center, useToast, MenuButton, Menu, IconButton, Flex } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
-import {TaskList} from './Components/TaskList/TaskList';
-import {TaskForm} from './Components/TaskForm/TaskForm';
+import { TaskList } from './Components/TaskList/TaskList';
+import { TaskForm } from './Components/TaskForm/TaskForm';
 
 export const App = () => {
-
   const [tasks, setTasks] = useState(() => {
     // Intenta obtener las tareas desde localStorage al cargar la aplicación
     const storedTasks = localStorage.getItem('tasks');
     return storedTasks ? JSON.parse(storedTasks) : [];
+  });
+
+  const [taskIdCounter, setTaskIdCounter] = useState(() => {
+    const storedIdCounter = localStorage.getItem('taskIdCounter');
+    return storedIdCounter ? parseInt(storedIdCounter, 10) : 1;
   });
 
   const [actionPerformed, setActionPerformed] = useState(false);
@@ -18,6 +22,7 @@ export const App = () => {
   // Guarda las tareas en localStorage cuando se actualiza el estado
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('taskIdCounter', taskIdCounter.toString());
     if (actionPerformed) {
       toast({
         title: 'Lista de tareas actualizada',
@@ -28,7 +33,7 @@ export const App = () => {
       });
       setActionPerformed(false);
     }
-  }, [tasks, actionPerformed, toast]);
+  }, [tasks, taskIdCounter, actionPerformed, toast]);
 
   const handleTaskCompleted = (taskId, isCompleted) => {
     const updatedTasks = tasks.map((task) =>
@@ -43,14 +48,15 @@ export const App = () => {
   };
 
   const handleAddTask = (taskName) => {
-    const newTask = {      
+    const newTask = {
+      id: taskIdCounter,
       name: taskName,
-      completed: false,      
+      completed: false,
     };
     setTasks([...tasks, newTask]);
+    setTaskIdCounter(taskIdCounter + 1);
     setActionPerformed(true);
   };
-  
 
   return (
     <Center height="100vh">
@@ -58,15 +64,15 @@ export const App = () => {
         <Flex minWidth='max-content' alignItems='center' gap='2'>
           <Box>
             <Menu isDisabled={true}>
-            <MenuButton
-              as={IconButton}          
-              aria-label='Menu'
-              icon={<HamburgerIcon color='white'/>}
-              variant='outline'
-              border='none'
-              _hover={{ bg: 'blue.700' }}
-              _active={{ bg: 'blue.700' }}  
-            />
+              <MenuButton
+                as={IconButton}
+                aria-label='Menu'
+                icon={<HamburgerIcon color='white' />}
+                variant='outline'
+                border='none'
+                _hover={{ bg: 'blue.700' }}
+                _active={{ bg: 'blue.700' }}
+              />
             </Menu>
           </Box>
           <Spacer />
--- todo-app/src/Components/TaskList/TaskList.jsx
+++ todo-app/src/Components/TaskList/TaskList.jsx
@@ -1,46 +1,52 @@
 import React from 'react';
-import { Flex, Heading, Box, Text, Center } from '@chakra-ui/react'
+import { Flex, Heading, Box, Text, Center } from '@chakra-ui/react';
 import { TaskItem } from '../TaskItem/TaskItem';
 
-export const TaskList = ({ tasks, onTaskCompleted, onDeleteTask }) => {
+export const TaskList = ({ tasks, onTaskCompleted, onDeleteTask, setTasks }) => {
+    const handleTaskCompleted = (taskId, isCompleted) => {
+      const updatedTasks = tasks.map((task) =>
+        task.id === taskId ? { ...task, completed: isCompleted } : task
+      );
+      setTasks(updatedTasks);
+    };
 
-    // Filtrar tareas por completar y tareas completadas
-    const incompleteTasks = tasks.filter((task) => !task.completed);
-    const completedTasks = tasks.filter((task) => task.completed);
+  // Filtrar tareas por completar y tareas completadas
+  const incompleteTasks = tasks.filter((task) => !task.completed);
+  const completedTasks = tasks.filter((task) => task.completed);
 
-    return (
-        <>
-            <Flex flexDirection="column" mt={4}>
-                {/* Lista de tareas por completar */}
-                <Heading size="xs" color='gray.300' mb={2}>
-                    POR HACER
-                </Heading>
-                {incompleteTasks.map((task) => (
-                    <TaskItem
-                        key={task.id}
-                        task={task}
-                        onTaskCompleted={onTaskCompleted}
-                        onDeleteTask={onDeleteTask}
-                    />
-                ))}
+  return (
+    <>
+      <Flex flexDirection="column" mt={4}>
+        {/* Lista de tareas por completar */}
+        <Heading size="xs" color='gray.300' mb={2}>
+          POR HACER
+        </Heading>
+        {incompleteTasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onTaskCompleted={onTaskCompleted}
+            onDeleteTask={onDeleteTask}
+          />
+        ))}
 
-                {/* Lista de tareas completadas */}
-                {completedTasks.length > 0 && (
-                    <>
-                        <Heading size="xs" color='gray.300' mt={4} mb={2}>
-                            COMPLETADAS
-                        </Heading>
-                        {completedTasks.map((task) => (
-                            <TaskItem
-                                key={task.id}
-                                task={task}
-                                onTaskCompleted={onTaskCompleted}
-                                onDeleteTask={onDeleteTask}
-                            />
-                        ))}
-                    </>
-                )}
-            </Flex>
-        </>
-    );
+        {/* Lista de tareas completadas */}
+        {completedTasks.length > 0 && (
+          <>
+            <Heading size="xs" color='gray.300' mt={4} mb={2}>
+              COMPLETADAS
+            </Heading>
+            {completedTasks.map((task) => (
+              <TaskItem
+                key={task.id}
+                task={task}
+                onTaskCompleted={onTaskCompleted}
+                onDeleteTask={onDeleteTask}
+              />
+            ))}
+          </>
+        )}
+      </Flex>
+    </>
+  );
 };
