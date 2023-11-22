641392fb8c00d5277a18586e1ce2e330cf126f3e
JFEMENIA <jorgefemenia@gmail.com>
2023-11-21 22:12:39 -0300

    quitado calendario de form

--- todo-app/src/Components/TaskForm/TaskForm.jsx
+++ todo-app/src/Components/TaskForm/TaskForm.jsx
@@ -1,28 +1,18 @@
 import React, { useState } from 'react';
 import { Flex, Input, Button, InputGroup, InputRightElement } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 
 export const TaskForm = ({ onAddTask }) => {
     const [taskName, setTaskName] = useState('');
-    const [showCalendar, setShowCalendar] = useState(false);
-    const [dueDate, setDueDate] = useState(new Date());
 
     const handleInputChange = (e) => {
         setTaskName(e.target.value);
     };
 
-    const handleDateChange = (date) => {
-        setDueDate(date);
-    };
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        onAddTask(taskName, dueDate);
+        onAddTask(taskName);
         setTaskName('');
-        setDueDate(new Date());
-        setShowCalendar(false);
     };
 
     return (
@@ -39,41 +29,18 @@ export const TaskForm = ({ onAddTask }) => {
                 />
                 <InputRightElement>
                     <Button
-                        type="button"
+                        type="submit"
                         variant="solid"
                         borderRadius="300px"
                         mr="5px"
                         colorScheme="teal"
                         size='sm'
                         fontSize="12px"
-                        onClick={() => setShowCalendar(!showCalendar)}
                     >
                         <AddIcon />
                     </Button>
                 </InputRightElement>
             </InputGroup>
-
-            {showCalendar && (
-                <Flex flexDirection="column" alignItems="center" color="black" ml="260px" mt="170px" position="absolute" zIndex="1">
-                    <Calendar
-                        onChange={handleDateChange}
-                        value={dueDate}
-                        minDate={new Date()}  
-
-                    />
-                    <Button
-                        type="button"
-                        variant="solid"
-                        colorScheme="teal"
-                        size='sm'
-                        fontSize="14px"
-                        onClick={handleSubmit}
-                        mt={2}  
-                    >
-                        Agregar 
-                    </Button>
-                </Flex>
-            )}
         </Flex>
     );
 };
